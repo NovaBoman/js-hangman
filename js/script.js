f17dccee705e49ad60f5900a0a2b2870f3f97905
Nova Boman <nova.boman@chasacademy.se>
2021-11-25 11:33:35 +0100

    Declare and define global variables

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Global Variables //
+const wordList = ["green", "red", "purple"]; // Word list array from which to choose random word
+let correctWord = ""; // Randomly chosen word from word list
+let currentWord = []; // Chosen word to be displayed to user
+let wrongGuesses = 0; // Tracker for amount of wrong guesses
+let lettersGuessed = []; // Tracker for guessed letters
