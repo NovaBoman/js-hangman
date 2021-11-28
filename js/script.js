febace187793c573ccfe1287c9847015e3f8b0fa
Nova Boman <nova.boman@chasacademy.se>
2021-11-28 17:42:28 +0100

    Add styling and update styling related functionality

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ let imageSource = `images/h${wrongGuesses}.png`;
 document.querySelector(".hangman-image").src = imageSource; // Sets the image source for hangman image
 document.querySelector(".new-game").addEventListener("click", startGame); // Event listener for start button
 document.querySelector(".guess-input").disabled = true; // Disables the input field until the game is started
-printMessage("Welcome to Hangman, press 'New Game' to start!");
+printMessage("Press 'New Game' to start!");
 
 // Starts the game //
 function startGame() {
@@ -18,31 +18,32 @@ function startGame() {
   lettersGuessed = [];
   wrongGuesses = 0;
   updateImage(wrongGuesses);
-  document.querySelector(".guessed-letters").innerHTML = "";
+  document.querySelector(".guessed-letters").innerHTML = "Guesses:";
+  document.querySelector(".chances-left").innerHTML = `Chances left: 
+  ${6 - wrongGuesses} / 6`;
   document.querySelector(".guess-input").disabled = false; // Enables input after being disabled when game is won or lost
-  printMessage("");
+  document.querySelector(".guess-input").focus();
+  printMessage("Guess a letter, A-Z");
 
   // Depending on the length of correctWord, fills currentWord array with placeholder '_' on each index
   for (let i = 0; i < correctWord.length; i++) {
     currentWord.push("_");
   }
   document.querySelector(".current-word").innerHTML = currentWord.join(" "); // Creates string from currentWord and displays to user using p-tag.
+  document.querySelector(".current-word").style.visibility = "visible";
 }
 
 document.querySelector(".guess-input").addEventListener("input", handleInput); // Event listener for input field
 
 // Handles user input in input field //
 function handleInput(e) {
-  printMessage("");
-  if (!lettersGuessed.includes(e.target.value)) {
-    if (e.target.value.match(/[a-z]/i)) {
-      // Defines a regular expression with a pattern to match input to, only letter a-z are allowed, flag i makes it case insensitive
-      // If the value of the event target (input field) matches the pattern run function guessLetter()
-      guessLetter(e.target.value);
-    } else {
-      // Handles cases where input does not match expected pattern
-      printMessage("Your guess must be a letter between A-Z");
-    }
+  if (
+    !lettersGuessed.includes(e.target.value) &&
+    e.target.value.match(/[a-z]/i)
+  ) {
+    // Defines a regular expression with a pattern to match input to, only letter a-z are allowed, flag i makes it case insensitive
+    // If the value of the input field is not already in the list of guessed letters and matches the pattern run function guessLetter()
+    guessLetter(e.target.value);
   }
   e.target.value = ""; // Clears input field
 }
@@ -79,7 +80,9 @@ function handleCorrectGuess(letter) {
 function handleWrongGuess() {
   wrongGuesses++;
   updateImage(wrongGuesses);
-  console.log(`wrong ${wrongGuesses} times`);
+  document.querySelector(".chances-left").innerHTML = `Chances left: 
+  ${6 - wrongGuesses} / 6`;
+
   if (wrongGuesses === 6) {
     document.querySelector(".guess-input").disabled = true; // Disable input when game is lost
     printMessage("You lost!");
