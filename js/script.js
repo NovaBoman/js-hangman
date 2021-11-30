b7d31b6d3098b3340baf3079ef6726eb852b0ab9
Nova Boman <nova.boman@chasacademy.se>
2021-11-30 12:50:16 +0100

    Format code, delete unused files, remove unused code, update comments

--- js/script.js
+++ js/script.js
@@ -1,100 +1,113 @@
-// Global Variables //
-const wordList = ["green", "red", "purple"]; // Word list array from which to choose random word
-let correctWord = ""; // Randomly chosen word from word list
-let currentWord = []; // Chosen word to be displayed to user
-let wrongGuesses = 0; // Tracker for amount of wrong guesses
-let lettersGuessed = []; // Tracker for guessed letters
-let imageSource = `images/h${wrongGuesses}.png`;
+/// Global Variables ///
+const wordList = ["green", "red", "purple"]; // Array from which correctWord is randomly chosen.
+let correctWord = "";
+let currentWord = []; // Word to be displayed to user.
+let wrongGuesses = 0; // Tracks number of wrong guesses.
+let guessedLetters = []; // Tracks letters guessed by user.
 
-document.querySelector(".hangman-image").src = imageSource; // Sets the image source for hangman image
-document.querySelector(".guess-input").addEventListener("input", handleInput); // Event listener for input field
-document.querySelector(".new-game").addEventListener("click", startGame); // Event listener for start button
-document.querySelector(".guess-input").disabled = true; // Disables the input field until the game is started
-printMessage("Press 'New Game' to start!");
+// Add event listeners for New Game button and input field.
+document.querySelector(".guess-input").addEventListener("input", handleInput);
+document.querySelector(".new-game").addEventListener("click", startGame);
 
-// Starts the game //
+updateImage();
+
+/// FUNCTIONS ///
+
+// Starts/restarts the game //
+/* Randomly selects the correct word then resets global variables. 
+   Updates text displayed to the user, enables and focuses input field.
+   Creates a string of placeholder "_" from the length of correctWord */
 function startGame() {
-  correctWord = wordList[Math.floor(Math.random() * wordList.length)]; // Assigns correctWord by randomly choosing a word from wordList
-  currentWord = []; // Resets variables every time a new game starts
-  lettersGuessed = [];
+  correctWord = wordList[Math.floor(Math.random() * wordList.length)]; // Assigns correctWord by randomly choosing a word from wordList.
+  currentWord = [];
+  guessedLetters = [];
   wrongGuesses = 0;
-  updateImage(wrongGuesses);
-  document.querySelector(".guessed-letters").innerHTML = "Guesses:";
-  document.querySelector(".chances-left").innerHTML = `Chances left: 
-  ${6 - wrongGuesses} / 6`;
-  document.querySelector(".guess-input").disabled = false; // Enables input after being disabled when game is won or lost
-  document.querySelector(".guess-input").focus();
-  printMessage("Guess a letter, A-Z");
+  updateImage();
 
-  // Depending on the length of correctWord, fills currentWord array with placeholder '_' on each index
+  updateMessage("Guess a letter, A-Z");
+  document.querySelector(".chances-left").textContent = `Chances left: 
+  ${6 - wrongGuesses} / 6`; // Resets counter for chances left.
+  document.querySelector(".guess-input").disabled = false; // Enable input when game starts/restarts.
+  document.querySelector(".guess-input").focus(); // Focus input field when game starts.
+  document.querySelector(".guessed-letters").textContent = "Guesses:";
+
+  // Fills currentWord array with a placeholder '_' on each index.
   for (let i = 0; i < correctWord.length; i++) {
     currentWord.push("_");
   }
-  document.querySelector(".current-word").innerHTML = currentWord.join(" "); // Creates string from currentWord and displays to user using p-tag.
+  document.querySelector(".current-word").textContent = currentWord.join(" "); // Creates string from currentWord and displays it to the user.
   document.querySelector(".current-word").style.visibility = "visible";
 }
 
 // Handles user input in input field //
+/*Defines a regular expression with a pattern to match input to, only letter a-z are allowed, flag i makes it case insensitive.
+  If the value of the input field is not already in the list of guessed letters and matches the pattern run function guessLetter() */
 function handleInput(e) {
   if (
-    !lettersGuessed.includes(e.target.value) &&
+    !guessedLetters.includes(e.target.value) &&
     e.target.value.match(/[a-z]/i)
   ) {
-    // Defines a regular expression with a pattern to match input to, only letter a-z are allowed, flag i makes it case insensitive
-    // If the value of the input field is not already in the list of guessed letters and matches the pattern run function guessLetter()
     guessLetter(e.target.value);
   }
-  e.target.value = ""; // Clears input field
+  e.target.value = ""; // Clears input field.
 }
 
-// Handles valid guesses // - Takes a value (set to value of input field above)
+// Handles valid guesses // - "letter" set to value of input field in function above.
+/* Transforms input to lowercase to allow user to input both upper and lowercase letters.
+   Checks if letter is included in correctWord and then runs function for correct or wrong guess. */
 function guessLetter(letter) {
-  letter = letter.toLowerCase(); // Transform to lowercase to allow user to input upper and lowercase letters
-
+  letter = letter.toLowerCase();
   if (correctWord.includes(letter)) {
-    handleCorrectGuess(letter); // If correct word includes guessed letter run function for correct guess
+    handleCorrectGuess(letter);
   } else {
-    handleWrongGuess(); // Else run function for wrong guess
+    handleWrongGuess();
   }
-  lettersGuessed.push(letter); // Updates array of guessed letters and returns the updated array
-  document.querySelector(".guessed-letters").innerHTML =
-    lettersGuessed.join(" ");
+
+  // Updates array and string of guessed letters.
+  guessedLetters.push(letter);
+  document.querySelector(".guessed-letters").textContent =
+    guessedLetters.join(" ");
 }
 
-// Handles correct guesses // - Looks for occurences of letter in correctWord and splices them into currentWord
+// Handles correct guesses //
+/* Iterates over array correctWord and looks for occurences of input letter, if found splices it into currentWord on current index.
+   Creates a string from currentWord and displays it to the user.
+   If the word displayed to the user is exactly equal to correctWord the user wins the game. */
 function handleCorrectGuess(letter) {
   for (let i = 0; i < correctWord.length; i++) {
     if (correctWord[i] === letter) {
-      currentWord.splice(i, 1, letter); // Uses variable i to track index number and exchanges '_' for letter in currentWord
+      currentWord.splice(i, 1, letter);
     }
   }
-  document.querySelector(".current-word").innerHTML = currentWord.join(" "); // Creates string from array and displays currentWord to user
+  document.querySelector(".current-word").textContent = currentWord.join(" ");
+
   if (currentWord.join("") === correctWord) {
-    document.querySelector(".guess-input").disabled = true; // Disable input when game is won
-    printMessage("You won!");
+    document.querySelector(".guess-input").disabled = true; // Input is disabled until user starts a New Game.
+    updateMessage("You won!");
   }
 }
 
 // Handles wrong guesses //
+/* Increments wrongGuesses and updates the image accordingly, the user loses after 6 wrong guesses. */
 function handleWrongGuess() {
   wrongGuesses++;
-  updateImage(wrongGuesses);
-  document.querySelector(".chances-left").innerHTML = `Chances left: 
-  ${6 - wrongGuesses} / 6`;
+  updateImage();
+  document.querySelector(".chances-left").textContent = `Chances left: 
+  ${6 - wrongGuesses} / 6`; // Counts down chances left.
 
   if (wrongGuesses === 6) {
-    document.querySelector(".guess-input").disabled = true; // Disable input when game is lost
-    printMessage("You lost!");
+    document.querySelector(".guess-input").disabled = true;
+    updateMessage("You lost!");
   }
 }
 
-// Updates image //
-function updateImage(number) {
-  imageSource = `images/h${number}.png`; // Changes the url for the image
-  document.querySelector(".hangman-image").src = imageSource; // Sets the image source attribute for the HTML element
+// Changes url for the image //
+function updateImage() {
+  let imageSource = `images/h${wrongGuesses}.png`;
+  document.querySelector(".hangman-image").src = imageSource;
 }
 
 // Prints messages to user //
-function printMessage(string) {
-  document.querySelector(".message-to-user").innerHTML = string;
+function updateMessage(string) {
+  document.querySelector(".message-to-user").textContent = string;
 }
